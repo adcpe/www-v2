e7e643be44db78c3c9a867597657d709d8402049
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-04 10:46:34 -0500

    Changed blog posts page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="blog-posts">
-    <h1>Latest Blog Posts</h1>
-    <div class="post-links">
+  <div class="min-h-full font-lato flex-1">
+    <h1 class="text-4xl font-bold text-center my-10">Latest Blog Posts</h1>
+    <div class="w-1/2 m-auto flex flex-col justify-center content-center">
       <nuxt-link
-        v-for="(post, index) in posts"
-        :key="index"
+        v-for="(post, i) in posts"
+        :key="i"
         :to="{ path: `/blog/${post.slug}` }"
+        class="hover:text-links my-2"
       >
-        <div class="post-link">
-          <h1>{{ post.title }}</h1>
+        <div>
+          <h1 class="text-2xl font-semibold">{{ post.title }}</h1>
           <span><b>Posted</b> {{ formatDate(post.publishedOn) }}</span>
           <span v-if="post.updatedOn">
             <b>Updated</b> {{ formatDate(post.updatedOn) }}</span
@@ -21,8 +22,6 @@
 
 <script>
 import { groq } from '@nuxtjs/sanity'
-// import groq from 'groq'
-// import sanity from '../../plugins/sanity'
 import formatDate from '../../plugins/formatDate'
 
 export default {
@@ -37,38 +36,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.blog-posts {
-  margin: 3rem auto;
-  width: 80%;
-
-  & > h1 {
-    margin-bottom: 1.5rem;
-    text-align: center;
-  }
-
-  .post-links {
-    margin: 0 auto;
-    width: 80%;
-
-    .post-link {
-      color: black;
-      margin: 0.5rem auto;
-      padding: 0.5rem 3rem;
-
-      & > h1 {
-        font-size: 1.2rem;
-      }
-
-      & > span {
-        font-size: 0.9rem;
-      }
-
-      &:hover {
-        color: #f48498;
-      }
-    }
-  }
-}
-</style>
