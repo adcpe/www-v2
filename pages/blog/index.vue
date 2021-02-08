4f392d285897a386d8c7ce7cc27db5b6a127da2c
Andrés Del Carpio <andresdc@andresdc.net>
2021-02-08 12:14:46 -0500

    Changed font sizes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-full font-lato flex-1">
-    <h1 class="text-4xl font-bold text-center my-10">Blog Posts</h1>
+    <h1 class="text-3xl font-bold text-center my-10">Blog Posts</h1>
     <div class="w-1/2 m-auto flex flex-col justify-center content-center">
       <nuxt-link
         v-for="(post, i) in posts"
@@ -9,7 +9,7 @@
         class="hover:text-links my-2"
       >
         <div>
-          <h1 class="text-2xl font-semibold">{{ post.title }}</h1>
+          <h1 class="text-xl font-semibold">{{ post.title }}</h1>
           <span><b>Posted</b> {{ formatDate(post.publishedOn) }}</span>
           <span v-if="post.updatedOn">
             <b>Updated</b> {{ formatDate(post.updatedOn) }}</span
